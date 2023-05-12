b2fb3d1edf5b88c2b841d9a6ea1af04004f7777f
bnina-ayoub <bnina.ayoub@etudiant-fst.utm.tn>
2023-05-12 16:05:29 +0100

    small changes

--- Face_test.py
+++ Face_test.py
@@ -124,7 +124,7 @@ while not Proceed:
     ret, frame = cap.read()
     fr = cv2.resize(frame, (0, 0), None, 0.25, 0.25)
     fr = cv2.cvtColor(fr, cv2.COLOR_BGR2RGB)
-    video_writer.write(fr)
+    video_writer.write(frame)
     faceCurentFrame = face_recognition.face_locations(fr)
     encodeCurentFrame = face_recognition.face_encodings(fr, faceCurentFrame)
     # print(len(encodeCurentFrame))
